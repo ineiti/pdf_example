db2448869213043a8c7cee7cf73d0c7af63bc66a
Linus Gasser <linus@gasser.blue>
2020-07-18 18:35:13 +0200

    Updating README

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
+import {getDocument} from "pdfjs-dist/tsc-out/display/api";
+import {GlobalWorkerOptions} from "pdfjs-dist/tsc-out/display/worker_options";
+
+import pdfjsWorker from "pdfjs-dist/build/pdf.worker";
+console.log("pdfjsworker:", pdfjsWorker);
+GlobalWorkerOptions.workerSrc = pdfjsWorker;
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  static pdfData = atob(
+    'JVBERi0xLjcKCjEgMCBvYmogICUgZW50cnkgcG9pbnQKPDwKICAvVHlwZSAvQ2F0YWxvZwog' +
+    'IC9QYWdlcyAyIDAgUgo+PgplbmRvYmoKCjIgMCBvYmoKPDwKICAvVHlwZSAvUGFnZXMKICAv' +
+    'TWVkaWFCb3ggWyAwIDAgMjAwIDIwMCBdCiAgL0NvdW50IDEKICAvS2lkcyBbIDMgMCBSIF0K' +
+    'Pj4KZW5kb2JqCgozIDAgb2JqCjw8CiAgL1R5cGUgL1BhZ2UKICAvUGFyZW50IDIgMCBSCiAg' +
+    'L1Jlc291cmNlcyA8PAogICAgL0ZvbnQgPDwKICAgICAgL0YxIDQgMCBSIAogICAgPj4KICA+' +
+    'PgogIC9Db250ZW50cyA1IDAgUgo+PgplbmRvYmoKCjQgMCBvYmoKPDwKICAvVHlwZSAvRm9u' +
+    'dAogIC9TdWJ0eXBlIC9UeXBlMQogIC9CYXNlRm9udCAvVGltZXMtUm9tYW4KPj4KZW5kb2Jq' +
+    'Cgo1IDAgb2JqICAlIHBhZ2UgY29udGVudAo8PAogIC9MZW5ndGggNDQKPj4Kc3RyZWFtCkJU' +
+    'CjcwIDUwIFRECi9GMSAxMiBUZgooSGVsbG8sIHdvcmxkISkgVGoKRVQKZW5kc3RyZWFtCmVu' +
+    'ZG9iagoKeHJlZgowIDYKMDAwMDAwMDAwMCA2NTUzNSBmIAowMDAwMDAwMDEwIDAwMDAwIG4g' +
+    'CjAwMDAwMDAwNzkgMDAwMDAgbiAKMDAwMDAwMDE3MyAwMDAwMCBuIAowMDAwMDAwMzAxIDAw' +
+    'MDAwIG4gCjAwMDAwMDAzODAgMDAwMDAgbiAKdHJhaWxlcgo8PAogIC9TaXplIDYKICAvUm9v' +
+    'dCAxIDAgUgo+PgpzdGFydHhyZWYKNDkyCiUlRU9G');
 
   constructor(private router: Router) { }
 
-  ngOnInit(): void { }
+  async ngOnInit() {
+    const doc = getDocument({data: HomeComponent.pdfData}) as any;
+    const pdf = doc.promise;
+    const page = await pdf.getPage(1);
+    console.log(page);
+    const pageContent = await page.getTextContent();
+    console.log(pageContent);
+    console.log(pageContent.items);
+    const pdfText: string = pageContent.items.map((i) => i.str).join(" ");
+    console.log("Text of PDF is: " + pdfText);
+  }
 
 }
